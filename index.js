35bac05ec81c41098c5a1475bf210fe5b968df1d
WooSangGyu <happywhn3@gmail.com>
2025-08-12 23:41:02 +0900

    ios 최신버전 작동 수정

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-'use strict';
+"use strict";
 
-var { DeviceEventEmitter, NativeModules, AppRegistry } = require('react-native');
+var {
+  DeviceEventEmitter,
+  NativeModules,
+  AppRegistry,
+} = require("react-native");
 var RNBackgroundGeolocation = NativeModules.BackgroundGeolocation;
-var TAG = 'RNBackgroundGeolocation';
-var TASK_KEY = 'com.marianhello.bgloc.react.headless.Task';
+var TAG = "RNBackgroundGeolocation";
+var TASK_KEY = "com.marianhello.bgloc.react.headless.Task";
 
 function emptyFn() {}
 function defaultErrorHandler(error) {
   var cause = error.cause || {};
   var causeMessage = cause.message;
-  throw TAG + ': ' + error.message + (causeMessage ? ': ' + cause.message : '');
+  throw TAG + ": " + error.message + (causeMessage ? ": " + cause.message : "");
 }
+
+/**
+ * Default config merged in `configure`.
+ * NOTE: `useSignificantChanges` is iOS-only (native patch required).
+ */
+var DEFAULT_CONFIG = {
+  // 기존 기본값이 있다면 여기로 옮겨서 관리하세요.
+  // 예: desiredAccuracy, distanceFilter 등
+  useSignificantChanges: false,
+};
 
 var BackgroundGeolocation = {
   events: [
-    'location',
-    'stationary',
-    'activity',
-    'start',
-    'stop',
-    'error',
-    'authorization',
-    'foreground',
-    'background',
-    'abort_requested',
-    'http_authorization'
+    "location",
+    "stationary",
+    "activity",
+    "start",
+    "stop",
+    "error",
+    "authorization",
+    "foreground",
+    "background",
+    "abort_requested",
+    "http_authorization",
   ],
 
   DISTANCE_FILTER_PROVIDER: 0,
@@ -43,41 +57,44 @@ var BackgroundGeolocation = {
   LOW_ACCURACY: 1000,
   PASSIVE_ACCURACY: 10000,
 
-  LOG_ERROR: 'ERROR',
-  LOG_WARN: 'WARN',
-  LOG_INFO: 'INFO',
-  LOG_DEBUG: 'DEBUG',
-  LOG_TRACE: 'TRACE',
+  LOG_ERROR: "ERROR",
+  LOG_WARN: "WARN",
+  LOG_INFO: "INFO",
+  LOG_DEBUG: "DEBUG",
+  LOG_TRACE: "TRACE",
 
   PERMISSION_DENIED: 1,
   LOCATION_UNAVAILABLE: 2,
   TIMEOUT: 3,
 
-  configure: function(config, successFn, errorFn) {
+  configure: function (config, successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || defaultErrorHandler;
-    RNBackgroundGeolocation.configure(config, successFn, errorFn);
+
+    // 새 기본값 병합 (스프레드가 없을 수도 있어 Object.assign 사용)
+    var merged = Object.assign({}, DEFAULT_CONFIG, config || {});
+    RNBackgroundGeolocation.configure(merged, successFn, errorFn);
   },
 
-  start: function() {
+  start: function () {
     RNBackgroundGeolocation.start();
   },
 
-  stop: function() {
+  stop: function () {
     RNBackgroundGeolocation.stop();
   },
 
-  checkStatus: function(successFn, errorFn) {
+  checkStatus: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.checkStatus(successFn, errorFn);
   },
 
-  showAppSettings: function() {
+  showAppSettings: function () {
     RNBackgroundGeolocation.showAppSettings();
   },
 
-  showLocationSettings: function() {
+  showLocationSettings: function () {
     RNBackgroundGeolocation.showLocationSettings();
   },
 
@@ -90,76 +107,90 @@ var BackgroundGeolocation = {
     RNBackgroundGeolocation.getStationaryLocation(successFn, errorFn);
   },
 
-  getCurrentLocation: function(successFn, errorFn, options) {
+  getCurrentLocation: function (successFn, errorFn, options) {
     options = options || {};
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.getCurrentLocation(options, successFn, errorFn);
   },
 
-  getLocations: function(successFn, errorFn) {
+  getLocations: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.getLocations(successFn, errorFn);
   },
 
-  getValidLocations: function(successFn, errorFn) {
+  getValidLocations: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.getValidLocations(successFn, errorFn);
   },
 
-  deleteLocation: function(locationId, successFn, errorFn) {
+  deleteLocation: function (locationId, successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.deleteLocation(locationId, successFn, errorFn);
   },
 
-  deleteAllLocations: function(successFn, errorFn) {
+  deleteAllLocations: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.deleteAllLocations(successFn, errorFn);
   },
 
-  switchMode: function(modeId, successFn, errorFn) {
+  switchMode: function (modeId, successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.switchMode(modeId, successFn, errorFn);
   },
 
-  getConfig: function(successFn, errorFn) {
+  getConfig: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.getConfig(successFn, errorFn);
   },
 
-  getLogEntries: function(limit, /* offset = 0, minLevel = "DEBUG", successFn = emptyFn, errorFn = emptyFn */) {
+  getLogEntries: function (
+    limit /*, offset = 0, minLevel = "DEBUG", successFn = emptyFn, errorFn = emptyFn */
+  ) {
     var acnt = arguments.length;
     var offset, minLevel, successFn, errorFn;
 
-    if (acnt > 1 && typeof arguments[1] == 'function') {
+    if (acnt > 1 && typeof arguments[1] == "function") {
       // backward compatibility
-      console.log('[WARN]: Calling deprecated variant of getLogEntries method.');
+      console.log(
+        "[WARN]: Calling deprecated variant of getLogEntries method."
+      );
       offset = 0;
       minLevel = BackgroundGeolocation.LOG_DEBUG;
       successFn = arguments[1] || emptyFn;
       errorFn = arguments[2] || emptyFn;
     } else {
       offset = acnt > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-      minLevel = acnt > 2 && arguments[2] !== undefined ? arguments[2] : BackgroundGeolocation.LOG_DEBUG;
-      successFn = acnt > 3 && arguments[3] !== undefined ? arguments[3] : emptyFn;
+      minLevel =
+        acnt > 2 && arguments[2] !== undefined
+          ? arguments[2]
+          : BackgroundGeolocation.LOG_DEBUG;
+      successFn =
+        acnt > 3 && arguments[3] !== undefined ? arguments[3] : emptyFn;
       errorFn = acnt > 4 && arguments[4] !== undefined ? arguments[4] : emptyFn;
     }
 
-    RNBackgroundGeolocation.getLogEntries(limit, offset, minLevel, successFn, errorFn);
+    RNBackgroundGeolocation.getLogEntries(
+      limit,
+      offset,
+      minLevel,
+      successFn,
+      errorFn
+    );
   },
 
-  startTask: function(callbackFn) {
-    if (typeof callbackFn !== 'function') {
-      throw 'RNBackgroundGeolocation: startTask requires callback function';
+  startTask: function (callbackFn) {
+    if (typeof callbackFn !== "function") {
+      throw "RNBackgroundGeolocation: startTask requires callback function";
     }
 
-    if (typeof RNBackgroundGeolocation.startTask === 'function') {
+    if (typeof RNBackgroundGeolocation.startTask === "function") {
       RNBackgroundGeolocation.startTask(callbackFn);
     } else {
       // android does not need background tasks so we invoke callbackFn directly
@@ -167,30 +198,30 @@ var BackgroundGeolocation = {
     }
   },
 
-  endTask: function(taskKey) {
-    if (typeof RNBackgroundGeolocation.endTask === 'function') {
+  endTask: function (taskKey) {
+    if (typeof RNBackgroundGeolocation.endTask === "function") {
       RNBackgroundGeolocation.endTask(taskKey);
     } else {
       // noop
     }
   },
 
-  headlessTask: function(task, successFn, errorFn) {
+  headlessTask: function (task, successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     AppRegistry.registerHeadlessTask(TASK_KEY, () => task);
     RNBackgroundGeolocation.registerHeadlessTask(successFn, errorFn);
   },
 
-  forceSync: function(successFn, errorFn) {
+  forceSync: function (successFn, errorFn) {
     successFn = successFn || emptyFn;
     errorFn = errorFn || emptyFn;
     RNBackgroundGeolocation.forceSync(successFn, errorFn);
   },
 
-  on: function(event, callbackFn) {
-    if (typeof callbackFn !== 'function') {
-      throw TAG + ': callback function must be provided';
+  on: function (event, callbackFn) {
+    if (typeof callbackFn !== "function") {
+      throw TAG + ": callback function must be provided";
     }
     if (this.events.indexOf(event) < 0) {
       throw TAG + ': Unknown event "' + event + '"';
@@ -199,21 +230,26 @@ var BackgroundGeolocation = {
     return DeviceEventEmitter.addListener(event, callbackFn);
   },
 
-  removeAllListeners: function(event) {
+  removeAllListeners: function (event) {
     if (!event) {
-      this.events.forEach(function(event) {
+      this.events.forEach(function (event) {
         DeviceEventEmitter.removeAllListeners(event);
       });
       return void 0;
     }
     if (this.events.indexOf(event) < 0) {
-      console.log('[WARN] ' + TAG + ': removeAllListeners for unknown event "' + event + '"');
-      return void 0;
+      console.log(
+        "[WARN] " +
+          TAG +
+          ': removeAllListeners for unknown event "' +
+          event +
+          '"'
+      );
+    } else {
+      DeviceEventEmitter.removeAllListeners(event);
     }
-
-    DeviceEventEmitter.removeAllListeners(event);
     return void 0;
-  }
+  },
 };
 
 module.exports = BackgroundGeolocation;
--- ios/RCTBackgroundGeolocation/RCTBackgroundGeolocation.m
+++ ios/RCTBackgroundGeolocation/RCTBackgroundGeolocation.m
@@ -20,14 +20,19 @@
 #import "MAURConfig.h"
 #import "MAURBackgroundGeolocationFacade.h"
 #import "MAURBackgroundTaskManager.h"
+#import <CoreLocation/CoreLocation.h> // (추가) 권한 상태 확인용
 
 #define isNull(value) value == nil || [value isKindOfClass:[NSNull class]]
 
 @implementation RCTBackgroundGeolocation {
     MAURBackgroundGeolocationFacade* facade;
-    
+
+    // (추가) iOS10+ 알림 델리게이트 유지용
     API_AVAILABLE(ios(10.0))
     __weak id<UNUserNotificationCenterDelegate> prevNotificationDelegate;
+
+    // (추가) JS configure에서 넘어온 옵션 저장
+    BOOL useSignificantChanges;
 }
 
 @synthesize bridge = _bridge;
@@ -41,7 +46,7 @@ RCT_EXPORT_MODULE();
     if (self) {
         facade = [[MAURBackgroundGeolocationFacade alloc] init];
         facade.delegate = self;
-        
+
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppPause:) name:UIApplicationDidEnterBackgroundNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppResume:) name:UIApplicationWillEnterForegroundNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onFinishLaunching:) name:UIApplicationDidFinishLaunchingNotification object:nil];
@@ -49,6 +54,9 @@ RCT_EXPORT_MODULE();
 
         // HACK: it seems to be too late to register on launch observer so trigger it manually
         [self onFinishLaunching:nil];
+
+        // (추가) 기본값
+        useSignificantChanges = NO;
     }
 
     return self;
@@ -56,14 +64,40 @@ RCT_EXPORT_MODULE();
 
 /**
  * configure plugin
+ *
+ * JS에서 넘어온 config에 `useSignificantChanges: boolean`을 지원.
+ * - 가능하면 facade/Config로 전달(메서드 존재 시)
  */
 RCT_EXPORT_METHOD(configure:(NSDictionary*)configDictionary success:(RCTResponseSenderBlock)success failure:(RCTResponseSenderBlock)failure)
 {
     RCTLogInfo(@"RCTBackgroundGeolocation #configure");
     MAURConfig* config = [MAURConfig fromDictionary:configDictionary];
     NSError *error = nil;
-    
+
+    // (추가) useSignificantChanges 파싱
+    id usc = [configDictionary objectForKey:@"useSignificantChanges"];
+    if (!isNull(usc)) {
+        useSignificantChanges = [usc boolValue];
+    } else {
+        useSignificantChanges = NO;
+    }
+
+    // (선택) MAURConfig가 속성을 지원한다면 KVC로 주입 (없어도 무시되도록 try/catch)
+    @try {
+        [config setValue:@(useSignificantChanges) forKey:@"useSignificantChanges"];
+    } @catch (NSException *exception) {
+        // ignore if key doesn't exist
+    }
+
     if ([facade configure:config error:&error]) {
+        // (추가) facade가 해당 API를 제공한다면 런타임 체크 후 전달
+        if ([facade respondsToSelector:@selector(setUseSignificantChanges:)]) {
+            // id<NSObject>로 캐스팅 후 performSelector는 ARC 경고가 있을 수 있어 pragma로 무시
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [facade performSelector:@selector(setUseSignificantChanges:) withObject:@(useSignificantChanges)];
+#pragma clang diagnostic pop
+        }
         success(@[[NSNull null]]);
     } else {
         NSDictionary *dict = [self errorToDictionary:MAURBGConfigureError message:@"Configuration error" cause:error];
@@ -74,6 +108,27 @@ RCT_EXPORT_METHOD(configure:(NSDictionary*)configDictionary success:(RCTResponse
 RCT_EXPORT_METHOD(start)
 {
     RCTLogInfo(@"RCTBackgroundGeolocation #start");
+
+    // (추가) iOS 권한 승격이 필요한 경우 보조 호출 (facade가 구현했다면)
+    if ([facade respondsToSelector:@selector(ensureAlwaysAuthorization)]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [facade performSelector:@selector(ensureAlwaysAuthorization)];
+#pragma clang diagnostic pop
+    } else {
+        // 최소한 권한 상태 로그 (디버깅 도움)
+        CLAuthorizationStatus st = [CLLocationManager authorizationStatus];
+        RCTLogInfo(@"RCTBackgroundGeolocation auth status (pre-start): %ld", (long)st);
+    }
+
+    // (추가) 시작 전에 significant-changes 모드 전달(가능하면)
+    if ([facade respondsToSelector:@selector(setUseSignificantChanges:)]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [facade performSelector:@selector(setUseSignificantChanges:) withObject:@(useSignificantChanges)];
+#pragma clang diagnostic pop
+    }
+
     NSError *error = nil;
     [facade start:&error];
 
@@ -195,7 +250,7 @@ RCT_EXPORT_METHOD(getCurrentLocation:(NSDictionary*)options success:(RCTResponse
         }
     });
 }
-                   
+
 RCT_EXPORT_METHOD(getStationaryLocation:(RCTResponseSenderBlock)success failure:(RCTResponseSenderBlock)failure)
 {
     RCTLogInfo(@"RCTBackgroundGeolocation #getStationaryLocation");
@@ -225,7 +280,13 @@ RCT_EXPORT_METHOD(getConfig:(RCTResponseSenderBlock)success failure:(RCTResponse
     if (config == nil) {
         config = [[MAURConfig alloc] init]; // default config
     }
-    success(@[[config toDictionary]]);
+
+    // (추가) 런타임에 facade/Config가 flag를 유지하지 않는 경우를 대비해 반영
+    NSMutableDictionary *dict = [[config toDictionary] mutableCopy];
+    if (dict == nil) { dict = [NSMutableDictionary new]; }
+    [dict setObject:@(useSignificantChanges) forKey:@"useSignificantChanges"];
+
+    success(@[dict]);
 }
 
 RCT_EXPORT_METHOD(checkStatus:(RCTResponseSenderBlock)success failure:(RCTResponseSenderBlock)failure)
@@ -240,6 +301,8 @@ RCT_EXPORT_METHOD(checkStatus:(RCTResponseSenderBlock)success failure:(RCTRespon
     [dict setObject:[NSNumber numberWithBool:locationServicesEnabled] forKey:@"hasPermissions"]; // @deprecated
     [dict setObject:[NSNumber numberWithBool:locationServicesEnabled] forKey:@"locationServicesEnabled"];
     [dict setObject:[NSNumber numberWithInteger:authorizationStatus] forKey:@"authorization"];
+    // (추가) 현재 설정된 significant-change 노출
+    [dict setObject:@(useSignificantChanges) forKey:@"useSignificantChanges"];
 
     success(@[dict]);
 }
@@ -313,7 +376,7 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
     NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : NSTemporaryDirectory();
-    
+
     return [basePath stringByAppendingPathComponent:@"SQLiteLogger"];
 }
 
@@ -376,7 +439,7 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
 - (void) onAbortRequested
 {
     RCTLogInfo(@"RCTBackgroundGeoLocation abort requested by the server");
-    
+
     if (_bridge)
     {
         [self sendEvent:@"abort_requested"];
@@ -390,7 +453,7 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
 - (void) onHttpAuthorization
 {
     RCTLogInfo(@"RCTBackgroundGeoLocation http authorization");
-    
+
     if (_bridge)
     {
         [self sendEvent:@"http_authorization"];
@@ -403,7 +466,7 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
 -(void) onFinishLaunching:(NSNotification *)notification
 {
     NSDictionary *dict = [notification userInfo];
-    
+
     MAURConfig *config = [facade getConfig];
     if (config.isDebugging)
     {
@@ -414,10 +477,17 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
             center.delegate = self;
         }
     }
-    
+
     if ([dict objectForKey:UIApplicationLaunchOptionsLocationKey]) {
         RCTLogInfo(@"RCTBackgroundGeolocation started by system on location event.");
         if (![config stopOnTerminate]) {
+            // (추가) 부팅/시스템 재기동으로 깨어났을 때도 flag 반영 시도
+            if ([facade respondsToSelector:@selector(setUseSignificantChanges:)]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+                [facade performSelector:@selector(setUseSignificantChanges:) withObject:@(useSignificantChanges)];
+#pragma clang diagnostic pop
+            }
             [facade start:nil];
             [facade switchMode:MAURBackgroundMode];
         }
@@ -431,7 +501,7 @@ RCT_EXPORT_METHOD(forceSync:(RCTResponseSenderBlock)success failure:(RCTResponse
     if (prevNotificationDelegate && [prevNotificationDelegate respondsToSelector:@selector(userNotificationCenter:willPresentNotification:withCompletionHandler:)])
     {
         // Give other delegates (like FCM) the chance to process this notification
-        
+
         [prevNotificationDelegate userNotificationCenter:center willPresentNotification:notification withCompletionHandler:^(UNNotificationPresentationOptions options) {
             completionHandler(UNNotificationPresentationOptionAlert);
         }];
